4c97cec5e00a1fbe63887e032ace80e6818a166e
Manas <manasck14@gmail.com>
2024-07-09 03:23:36 +0530

    feat: added more songs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// INITIALIZING AOS ANIMATION ON START
+AOS.init();
+
 const musicData = [
     {
         title: "Faded",
@@ -6,10 +9,58 @@ const musicData = [
         audioSrc: "assets/song/song1.m4a"
     },
     {
-        title: "Faded",
-        description: "Alan Walker",
-        imgSrc: "assets/image/img1.jpg",
-        audioSrc: "assets/song/song1.m4a"
+        title: "Into Your Arms",
+        description: "Ava Max and Witt Lowry",
+        imgSrc: "assets/image/img2.jpg",
+        audioSrc: "assets/song/song2.m4a"
+    },
+    {
+        title: "The Weeknd",
+        description: "Abel Makkonen Tesfaye",
+        imgSrc: "assets/image/img3.jpg",
+        audioSrc: "assets/song/song3.m4a"
+    },
+    {
+        title: "Shape of You",
+        description: "Ed Sheeran",
+        imgSrc: "assets/image/img4.jpg",
+        audioSrc: "assets/song/song4.m4a"
+    },
+    {
+        title: "Closer",
+        description: "The Chainsmokers",
+        imgSrc: "assets/image/img5.jpg",
+        audioSrc: "assets/song/song5.m4a"
+    },
+    {
+        title: "Love Me Like You Do",
+        description: "Ellie Goulding",
+        imgSrc: "assets/image/img6.jpg",
+        audioSrc: "assets/song/song6.m4a"
+    },
+    {
+        title: "Do Din",
+        description: "Darshan Raval",
+        imgSrc: "assets/image/img7.jpg",
+        audioSrc: "assets/song/song7.m4a"
+    },
+    {
+        title: "Chhod Diya",
+        description: "Arijit Singh, Kanika Kapoor",
+        imgSrc: "assets/image/img8.jpg",
+        audioSrc: "assets/song/song8.m4a"
+    },
+    {
+        title: "Janiye",
+        description: "Vishal Mishra",
+        imgSrc: "assets/image/img9.jpg",
+        audioSrc: "assets/song/song9.m4a"
+    },
+    {
+        title: "Dhundhala",
+        description: "Yashraj, Talwiinder",
+        imgSrc: "assets/image/img10.jpg",
+        audioSrc: "assets/song/song10.m4a"
     },
 
 ];
@@ -17,6 +68,10 @@ const musicData = [
 let currentIndex = 0;
 
 let likedSongs = JSON.parse(localStorage.getItem('likedSongs')) || [];
+
+window.onload = () => {
+    displayCard(currentIndex);
+};
 
 const musicContainer = document.getElementById('musicContainer');
 
@@ -28,12 +83,14 @@ function displayCard(index) {
 
     const musicCard = document.createElement('div');
     musicCard.className = 'musicCard';
+    musicCard.setAttribute('data-aos', 'zoom-in');
 
     musicCard.innerHTML = `
         <nav>
             <div class="circle" onclick="toggleLike(${index})">
-                <i id="heartIcon-${index}" class='fa${isLiked ? 's' : 'r'} fa-heart' style='color: ${isLiked ? 'red' : 'transparent'}'></i>
+                <i id="heartIcon-${index}" class="fa-regular fa-heart"></i>
             </div>
+            <p>Playing Now..</p>
             <div class="circle">
                 <i class='bx bx-menu'></i>
             </div>
@@ -63,6 +120,17 @@ function displayCard(index) {
     const progress = document.getElementById(`progress-${index}`);
     const durationDiv = document.getElementById(`duration-${index}`);
     const imageContainer = document.getElementById(`imageContainer-${index}`);
+    const heartIcon = document.getElementById(`heartIcon-${index}`);
+
+    if (isLiked) {
+        heartIcon.classList.remove('far');
+        heartIcon.classList.add('fas');
+        heartIcon.style.color = 'red';
+    } else {
+        heartIcon.classList.remove('fas');
+        heartIcon.classList.add('far');
+        heartIcon.style.color = '#86898c';
+    }
 
     song.onloadedmetadata = function () {
         progress.max = song.duration;
@@ -71,22 +139,13 @@ function displayCard(index) {
 
     song.ontimeupdate = function () {
         progress.value = song.currentTime;
-        durationDiv.textContent = formatTime(song.currentTime);
+        durationDiv.textContent = formatTime(song.duration - song.currentTime); // Display remaining time
+        updateProgressBackground(progress);
     };
-    
-    // song.onloadedmetadata = function () {
-    //     progress.max = song.duration;
-    //     progress.value = song.currentTime;
-    //     durationDiv.textContent = formatTime(song.currentTime);
-    // };
-
-    // song.ontimeupdate = function () {
-    //     progress.value = song.currentTime;
-    //     durationDiv.textContent = formatTime(song.currentTime);
-    // };
 
     progress.addEventListener('input', function() {
         song.currentTime = progress.value;
+        updateProgressBackground(progress);
     });
 
     song.onplay = function () {
@@ -128,14 +187,14 @@ function toggleLike(index) {
     
     if (likedSongs.includes(index)) {
         likedSongs = likedSongs.filter(i => i !== index);
-        heartIcon.classList.remove('fa');
+        heartIcon.classList.remove('fas');
         heartIcon.classList.add('far');
-        heartIcon.style.backgroundColor = 'transparent';
+        heartIcon.style.color = '#86898c';
     } else {
         likedSongs.push(index);
         heartIcon.classList.remove('far');
-        heartIcon.classList.add('fa');
-        heartIcon.style.backgroundColor = 'red';
+        heartIcon.classList.add('fas');
+        heartIcon.style.color = 'red';
     }
 
     localStorage.setItem('likedSongs', JSON.stringify(likedSongs));
@@ -145,6 +204,12 @@ function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+function updateProgressBackground(progress) {
+    const percentage = (progress.value / progress.max) * 100;
+    progress.style.setProperty('--progress', percentage + '%');
+    progress.style.setProperty('background', `linear-gradient(to right, #bf2311 ${percentage}%, var(--bg04) ${percentage}%)`);
 }
 
 displayCard(currentIndex);
